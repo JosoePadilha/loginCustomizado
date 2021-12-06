e7a881ade2be10da0a5de282c0d0fa6197a1c923
Josoé Simão Padilha <josopadilha@gmail.com>
2021-12-05 22:35:39 -0300

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,13 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<<<<<<< HEAD
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
         integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-=======
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
->>>>>>> b3b06c9e26e523d06e4fdf7b8da1e53303f262e0
     <title>{{ config('app.name') }}</title>
 </head>
 
@@ -45,11 +42,8 @@
         E-mail
         <input name="email" required type="email">
         Senha
-<<<<<<< HEAD
         <input name="password" required type="password">
-=======
         <input required type="password">
->>>>>>> b3b06c9e26e523d06e4fdf7b8da1e53303f262e0
         <button class="btn-primary" type="submit">Login</button>
     </form>
 
